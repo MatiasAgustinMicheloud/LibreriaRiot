e3709660bc826ac9e5d02ec724f4c36b8c47f14e
MatiasAgustinMicheloud <micheloud.matias@gmail.com>
2024-09-06 14:50:16 -0300

    Formulario Login y MenuAdmin

---
+++ LibreriaRiot/Principal/Login.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace LibreriaRiot.Principal
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtPassword = new TextBox();
+            txtUsuario = new TextBox();
+            btnIniciar = new Button();
+            lblPassword = new Label();
+            lblUsuario = new Label();
+            pictureBox1 = new PictureBox();
+            panel1 = new Panel();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // txtPassword
+            // 
+            txtPassword.Location = new Point(22, 81);
+            txtPassword.MaxLength = 20;
+            txtPassword.Name = "txtPassword";
+            txtPassword.Size = new Size(178, 23);
+            txtPassword.TabIndex = 9;
+            txtPassword.UseSystemPasswordChar = true;
+            // 
+            // txtUsuario
+            // 
+            txtUsuario.Location = new Point(22, 30);
+            txtUsuario.MaxLength = 50;
+            txtUsuario.Name = "txtUsuario";
+            txtUsuario.Size = new Size(178, 23);
+            txtUsuario.TabIndex = 8;
+            // 
+            // btnIniciar
+            // 
+            btnIniciar.Location = new Point(22, 129);
+            btnIniciar.Name = "btnIniciar";
+            btnIniciar.Size = new Size(75, 23);
+            btnIniciar.TabIndex = 7;
+            btnIniciar.Text = "Ingresar";
+            btnIniciar.UseVisualStyleBackColor = true;
+            btnIniciar.Click += btnIniciar_Click;
+            // 
+            // lblPassword
+            // 
+            lblPassword.AutoSize = true;
+            lblPassword.Location = new Point(22, 63);
+            lblPassword.Name = "lblPassword";
+            lblPassword.Size = new Size(67, 15);
+            lblPassword.TabIndex = 6;
+            lblPassword.Text = "Contraseña";
+            // 
+            // lblUsuario
+            // 
+            lblUsuario.AutoSize = true;
+            lblUsuario.Location = new Point(22, 12);
+            lblUsuario.Name = "lblUsuario";
+            lblUsuario.Size = new Size(47, 15);
+            lblUsuario.TabIndex = 5;
+            lblUsuario.Text = "Usuario";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(13, 14);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(82, 73);
+            pictureBox1.TabIndex = 10;
+            pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.ActiveCaption;
+            panel1.Controls.Add(txtPassword);
+            panel1.Controls.Add(txtUsuario);
+            panel1.Controls.Add(btnIniciar);
+            panel1.Controls.Add(lblPassword);
+            panel1.Controls.Add(lblUsuario);
+            panel1.Location = new Point(114, 42);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(215, 196);
+            panel1.TabIndex = 11;
+            // 
+            // Login
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(376, 288);
+            Controls.Add(panel1);
+            Controls.Add(pictureBox1);
+            Name = "Login";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Login";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TextBox txtPassword;
+        private TextBox txtUsuario;
+        private Button btnIniciar;
+        private Label lblPassword;
+        private Label lblUsuario;
+        private PictureBox pictureBox1;
+        private Panel panel1;
+    }
+}
---
+++ LibreriaRiot/Principal/Login.cs
@@ -0,0 +1,76 @@
+﻿using LibreriaRiot.Principal.Administrador;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LibreriaRiot.Principal
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private bool ValidacionDatos()
+        {
+            if (string.IsNullOrWhiteSpace(txtUsuario.Text))
+            {
+                MessageBox.Show("El campo 'Usuario' es obligatorio.", "Validación", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtUsuario.Focus(); // Focalizar el campo vacío
+                return false;
+            }
+
+            string emailPattern = @"^[^@\s]+@[^@\s]+\.[^@\s]+$";
+
+            if (!Regex.IsMatch(txtUsuario.Text, emailPattern))
+            {
+                MessageBox.Show("El formato del email es inválido.");
+                return false;
+            }
+
+
+            if (string.IsNullOrWhiteSpace(txtPassword.Text))
+            {
+                MessageBox.Show("El campo 'Contraseña' es obligatorio.", "Validación", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtPassword.Focus();
+                return false;
+            }
+            else
+            {
+                if (txtPassword.Text.Length < 3)
+                {
+                    MessageBox.Show("La contraseña debe tener al menos 3 caracteres.", "Validación", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return false;
+                }
+                // Verificar que contenga al menos una letra mayúscula, una minúscula y un número
+                if (!txtPassword.Text.Any(char.IsUpper) || !txtPassword.Text.Any(char.IsLower) || !txtPassword.Text.Any(char.IsDigit))
+                {
+                    MessageBox.Show("La contraseña debe contener al menos una letra mayúscula, una minúscula y un número.", "Validación", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private void btnIniciar_Click(object sender, EventArgs e)
+        {
+            ValidacionDatos();
+
+            if (txtUsuario.Text == "admin@gmail.com" && txtPassword.Text == "Admin1")
+            {
+                MessageBox.Show("Admin");
+                MenuAdmin menuAdmin = new MenuAdmin();
+                menuAdmin.Show();
+            }
+        }
+    }
+}
