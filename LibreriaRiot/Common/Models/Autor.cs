5ee1cbe11e896b6314675823f9d3bfa3c3921487
Matias <micheloud.matias@gmail.com>
2024-10-22 22:22:50 -0300

    se agrego los models de producto y datosProductos

---
+++ LibreriaRiot/Common/Models/Autor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibreriaRiot.Common.Models
+{
+    internal class Autor
+    {
+        public int Id_Autor { get; set; }
+        public string? Nombre { get; set; }
+    }
+}
